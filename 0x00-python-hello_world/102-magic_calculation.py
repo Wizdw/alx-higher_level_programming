cce2cf271f470b9491861197114de41c0ea132b8
root <wisdom700eduviere@gmail.com>
2023-03-08 03:27:40 -0800

    high level programming

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,3 @@
+102-magic_calculation.py#!/usr/bin/python3
+def magic_calculation(a, b):
+    return (98 + a ** b)102-magic_calculation.py
---
+++ README.md
@@ -0,0 +1,1 @@
+this is the readme for my high level programming projects
